c10e78adccbdfd521f16fa563588c046377de387
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2024-10-24 00:51:15 -0300

    finalizando crud da API e iniciando controllers controller.user.js e repositories repository.user.js

--- src/controllers/controller.doctor.js
+++ src/controllers/controller.doctor.js
@@ -2,9 +2,53 @@ import serviceDoctor from "../services/service.doctor.js";
 
 async function Listar(req, res) {
 
-    const doctors = await serviceDoctor.Listar();
+    const name = req.query.name;
+    const doctors = await serviceDoctor.Listar(name);
 
     res.status(200).json(doctors);
 }
 
-export default { Listar }
+async function Inserir(req, res) {
+
+    /* também pode ser feito assim, acessando as infos do corpo
+    da requisição um a um. A forma mais verborrajica de fazer...
+    
+    const name = req.body.name;
+    const specialty = req.body.specialty;
+    const icon = req.body.icon;
+    */
+    const { name, specialty, icon } = req.body;
+
+    const doctor = await serviceDoctor.Inserir(name, specialty, icon);
+
+    res.status(201).json(doctor);
+}
+
+async function Editar(req, res) {
+
+    const id_doctor = req.params.id_doctor;
+    const { name, specialty, icon } = req.body;
+
+    const doctor = await serviceDoctor.Editar(id_doctor, name, specialty, icon);
+
+    res.status(200).json(doctor);
+}
+
+async function Excluir(req, res) {
+
+    const id_doctor = req.params.id_doctor;
+
+    const doctor = await serviceDoctor.Excluir(id_doctor);
+
+    res.status(200).json(doctor);
+}
+
+async function ListarServicos(req, res) {
+
+    const id_doctor = req.params.id_doctor;
+    const serv = await serviceDoctor.ListarServicos(id_doctor);
+
+    res.status(200).json(serv);
+}
+
+export default { Listar, Inserir, Editar, Excluir, ListarServicos }
